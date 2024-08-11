fc6a2db99d2b9ca130304da013656de873717d81
Rajganez <ganezsouth@live.com>
2024-08-11 14:19:04 +0530

    Optimized codes and image view onclick the image in message body is rendered

--- src/pages/chats/MessageBody.jsx
+++ src/pages/chats/MessageBody.jsx
@@ -6,80 +6,80 @@ import moment from "moment";
 import "../../App.css";
 import { IoDownload } from "react-icons/io5";
 import { FaFolder } from "react-icons/fa";
-// import { useSocket } from "../../context/SocketContext.jsx";
 
 const MessageBody = () => {
-  //Event handling for scrolling to the bottom of the page when new messages are received
+  // Event handling for scrolling to the bottom of the page when new messages are received
   const scrollRef = useRef();
-  //Redux state variable to get the actions
+  
+  // Redux state variables to get the actions
   const message = useSelector((state) => state.socket.messages);
   const buddyiesInfo = useSelector((state) => state.emailverify);
-  //State to Render messages from the DB
+  
+  // State to Render messages from the DB
   const [messagesfromDB, setMessage] = useState([]);
-  //Error to handle new user sends messages
+  
+  // Error to handle new user sends messages
   const [error, setError] = useState(false);
-  //State to show the Shared Image in Zoom in mode
+  
+  // State to show the Shared Image in Zoom-in mode
   const [imageView, setImage] = useState(null);
-  //Function to handle the Image properties
-  const hideImage = () => setImage(null);
-  const showImage = (img) => {
-    setImage(img);
-  };
 
-  //Using Callback to Render the Message from the DB
-  const getMsgfromDB = useCallback(() => {
-    const getMsg = async () => {
-      if (buddyiesInfo.buddyId && buddyiesInfo.fellowId) {
-        try {
-          const response = await clientAPI.post(
-            GET_DM,
-            {
-              buddyId: buddyiesInfo.buddyId,
-              fellowId: buddyiesInfo.fellowId,
-            },
-            { withCredentials: true }
-          );
-          if (response.status === 200) {
-            setError(true);
-            setMessage(response.data.messages);
-          } else {
-            console.log("Error fetching messages");
-          }
-        } catch (error) {
-          if (error.response && error.response.status === 404) {
-            setError(false);
-            alert("start new chat");
-          } else {
-            alert("start new chat by sending a Hi message");
-            console.log(error);
-          }
+  // Function to handle the Image properties
+  const hideImage = () => setImage(null);
+  const showImage = (img) => setImage(img);
+
+  // Using Callback to Render the Message from the DB
+  const getMsgfromDB = useCallback(async () => {
+    if (buddyiesInfo.buddyId && buddyiesInfo.fellowId) {
+      try {
+        const response = await clientAPI.post(
+          GET_DM,
+          {
+            buddyId: buddyiesInfo.buddyId,
+            fellowId: buddyiesInfo.fellowId,
+          },
+          { withCredentials: true }
+        );
+        if (response.status === 200) {
+          setError(true);
+          setMessage(response.data.messages);
+        } else {
+          console.log("Error fetching messages");
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setError(false);
+          alert("Start new chat");
+        } else {
+          alert("Start new chat by sending a Hi message");
+          console.log(error);
         }
       }
-    };
-    getMsg();
+    }
   }, [buddyiesInfo]);
 
-  //To Call the API on each message received
+  // To Call the API on each message received
   useEffect(() => {
     getMsgfromDB();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [buddyiesInfo.fellowId, message]);
 
-  //Scrolling on end of the page
+  // Scrolling on end of the page
   useEffect(() => {
     if (scrollRef.current) {
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messagesfromDB]);
 
-  //Function to set the date using moment.js using the timestamp of the message from the DB
+  // Function to set the date using moment.js using the timestamp of the message from the DB
   const renderMsg = () => {
     let lastDate = null;
-    return messagesfromDB.map((msg, ind) => {
+    return messagesfromDB.map((msg) => {
       const messageData = moment(msg.timestamp).format("YYYY-MM-DD");
       const showDate = messageData !== lastDate;
+      lastDate = messageData;
       return (
-        <div key={ind}>
+        <div key={msg.timestamp}>
           {showDate && (
             <div className="d-flex justify-content-center">
               <div className="badge text-center fw-light text-bg-light my-1">
@@ -92,11 +92,12 @@ const MessageBody = () => {
       );
     });
   };
-  //For the Sent and Received Documents, Download Function
+
+  // For the Sent and Received Documents, Download Function
   const handleDownload = async (fileUrl, fileName) => {
     try {
-      const response = await fetch(`${fileUrl}`, {
-        withCredentials: true,
+      const response = await fetch(fileUrl, {
+        credentials: "include",
       });
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -123,136 +124,77 @@ const MessageBody = () => {
     return filePath.split("/").pop();
   };
 
-  //Function to Seperate the Sender and Recipient and different action for the different fileTypes
-  const renderDM = (msg) =>
-    msg.senderId === buddyiesInfo.buddyId ? (
-      <div key={msg._id}>
-        <div className="d-flex justify-content-end mb-2">
-          <div
-            className="d-inline-block text-dark text-end bg-primary-subtle border
-            rounded-start border-info text-break p-2 mt-1"
-          >
-            {(msg.messageType === "file" &&
-              (/\.(jpg|png|jpeg|gif|bmp|tiff|webp|svg|ico|heic|heif)$/i.test(
-                msg.content
-              ) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <img
-                    src={`${msg.content}`}
-                    width="200px"
-                    height="200px"
-                    onClick={() => showImage(msg.content)}
-                  />
-                </div>
-              ) : /\.(mp4)$/i.test(msg.content) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <video
-                    src={`${msg.content}`}
-                    width="200px"
-                    height="200px"
-                    controls
-                  />
-                </div>
-              ) : /\.(pdf|doc|docx|xls|xlsx|ppt|pptx|txt)$/i.test(
-                  msg.content
-                ) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <span
-                    onClick={() =>
-                      handleDownload(msg.content, extractFileName(msg.content))
-                    }
-                    style={{ display: "flex", alignItems: "center" }}
-                  >
-                    <FaFolder className="fs-5" style={{ marginRight: "8px" }} />
-                    {extractFileName(msg.content)}
-                    <IoDownload
-                      className="fs-5"
-                      style={{ marginLeft: "8px" }}
-                    />
-                  </span>
-                </div>
-              ) : (
-                msg.content
-              ))) ||
-              msg.content}
-          </div>
-        </div>
-        <div className="d-flex justify-content-end text-secondary font-size-timestamp">
-          {moment(msg.timestamp).format("LT")}
+  // Function to Separate the Sender and Recipient and different action for the different fileTypes
+  const renderDM = (msg) => (
+    <div key={msg.timestamp}>
+      <div
+        className={`d-flex ${msg.senderId === buddyiesInfo.buddyId ? 'justify-content-end' : 'justify-content-start'} mb-2`}
+      >
+        <div
+          className={`d-inline-block text-dark ${msg.senderId === buddyiesInfo.buddyId ? 'text-end bg-primary-subtle border rounded-start border-info' : 'text-start bg-light border rounded-end border border-secondary'} text-break p-2 mt-1`}
+        >
+          {msg.messageType === "file" ? (
+            (/\.(jpg|png|jpeg|gif|bmp|tiff|webp|svg|ico|heic|heif)$/i.test(msg.content) ? (
+              <div style={{ cursor: "pointer" }}>
+                <img
+                  src={msg.content}
+                  width="200px"
+                  height="200px"
+                  onClick={() => showImage(msg.content)}
+                  alt="Preview"
+                />
+              </div>
+            ) : /\.(mp4)$/i.test(msg.content) ? (
+              <div style={{ cursor: "pointer" }}>
+                <video
+                  src={msg.content}
+                  width="200px"
+                  height="200px"
+                  controls
+                />
+              </div>
+            ) : /\.(pdf|doc|docx|xls|xlsx|ppt|pptx|txt)$/i.test(msg.content) ? (
+              <div style={{ cursor: "pointer" }}>
+                <span
+                  onClick={() => handleDownload(msg.content, extractFileName(msg.content))}
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <FaFolder className="fs-5" style={{ marginRight: "8px" }} />
+                  {extractFileName(msg.content)}
+                  <IoDownload className="fs-5" style={{ marginLeft: "8px" }} />
+                </span>
+              </div>
+            ) : (
+              msg.content
+            ))
+          ) : (
+            msg.content
+          )}
         </div>
       </div>
-    ) : (
-      <div key={msg._id}>
-        <div className="d-flex justify-content-start mb-2">
-          <div
-            className="d-inline-block text-dark text-start bg-light border
-            rounded-end border border-secondary text-break p-2 mt-1"
-          >
-            {(msg.messageType === "file" &&
-              (/\.(jpg|png|jpeg|gif|bmp|tiff|webp|svg|ico|heic|heif)$/i.test(
-                msg.content
-              ) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <img
-                    src={`${msg.content}`}
-                    width="200px"
-                    height="200px"
-                    onClick={() => showImage(msg.content)}
-                  />
-                </div>
-              ) : /\.(mp4)$/i.test(msg.content) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <video
-                    src={`${msg.content}`}
-                    width="200px"
-                    height="200px"
-                    controls
-                  />
-                </div>
-              ) : /\.(pdf|doc|docx|xls|xlsx|ppt|pptx|txt)$/i.test(
-                  msg.content
-                ) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <span
-                    onClick={() =>
-                      handleDownload(msg.content, extractFileName(msg.content))
-                    }
-                    style={{ display: "flex", alignItems: "center" }}
-                  >
-                    <FaFolder className="fs-5" style={{ marginRight: "8px" }} />
-                    {extractFileName(msg.content)}
-                    <IoDownload
-                      className="fs-5"
-                      style={{ marginLeft: "8px" }}
-                    />
-                  </span>
-                </div>
-              ) : (
-                msg.content
-              ))) ||
-              msg.content}
-          </div>
-        </div>
-        <div className="d-flex justify-content-start text-secondary font-size-timestamp">
-          {moment(msg.timestamp).format("LT")}
-        </div>
-        {imageView !== null && (
-          <div
-            className="position-fixed start-0 d-flex align-items-center 
+      <div
+        className={`d-flex ${msg.senderId === buddyiesInfo.buddyId ? 'justify-content-end' : 'justify-content-start'} text-secondary font-size-timestamp`}
+      >
+        {moment(msg.timestamp).format("LT")}
+      </div>
+      {imageView !== null && (
+        <div
+          className="position-fixed start-0 d-flex align-items-center 
           justify-content-center custom-z-index start-50-above-425px"
-          >
-            {/* To show the image view in the interface */}
-            <div className="position-relative">
-              <img src={imageView} className="img-fluid" alt="Full Size" />
-              <button
-                className="btn btn-close position-absolute top-0 end-0 m-3"
-                onClick={hideImage}
-              />
-            </div>
+        >
+          {/* To show the image view in the interface */}
+          <div className="position-relative">
+            <img src={imageView} className="img-fluid" alt="Full Size" />
+            <button
+              className="btn btn-close position-absolute top-0 end-0 m-3"
+              onClick={hideImage}
+              aria-label="Close"
+            />
           </div>
-        )}
-      </div>
-    );
+        </div>
+      )}
+    </div>
+  );
 
   return (
     <div
@@ -260,7 +202,7 @@ const MessageBody = () => {
     scrollbar-hidden text-white"
     >
       {/* If the sender chats for the first time then condition is rendered */}
-      {error === true ? renderMsg() : <h6>Start New Chat</h6>}
+      {error ? renderMsg() : <h6>Start New Chat</h6>}
       <div ref={scrollRef} />
     </div>
   );
--- src/pages/groupchats/GroupMessageBody.jsx
+++ src/pages/groupchats/GroupMessageBody.jsx
@@ -6,22 +6,27 @@ import "../../App.css";
 import { IoDownload } from "react-icons/io5";
 import { FaFolder } from "react-icons/fa";
 
-//The Group chats are handled differently from the DM and mongoDB storage is in different method
+// The GroupMessageBody component handles group chat messages and displays them
 const GroupMessageBody = () => {
   //Page Scrolling behaviour handled by this event
   const scrollRef = useRef();
-  //Fetching the messages Redux stateto render the Page
+  //Fetching the messages Redux state to render the Page
   const message = useSelector((state) => state.socket.rooms);
-  //User Redux State variable
   const buddyiesInfo = useSelector((state) => state.emailverify);
-  //Render the Group Message from the DB
+  //State to Render the Group Message from the DB
   const [messagesfromDB, setMessage] = useState([]);
   //Error handling for member not in group
   const [error, setError] = useState(false);
+  //Function to handle the Image properties
+  //State to show the Image in Zoom in mode
+  const [imageView, setImage] = useState(null);
 
-  //API call to show the Group Message done using the callBack Hook
+  const hideImage = () => setImage(null);
+  const showImage = (img) => setImage(img);
+
+  // Fetch messages from the server
   const getGroupMsgfromDB = useCallback(() => {
-    if (buddyiesInfo.groupId || buddyiesInfo.groupId !== null) {
+    if (buddyiesInfo.groupId) {
       const getGroupChat = async () => {
         try {
           const response = await clientAPI.post(
@@ -36,10 +41,10 @@ const GroupMessageBody = () => {
             setMessage(response.data.nickName);
             setError(true);
           } else {
-            console.log("Error fetching messages");
+            console.error("Error fetching messages");
           }
         } catch (error) {
-          if (error.response.status === 404 && error.response.status) {
+          if (error.response?.status === 404) {
             setError(false);
           }
         }
@@ -48,29 +53,23 @@ const GroupMessageBody = () => {
     }
   }, [buddyiesInfo]);
 
-  //On Each message in the Room then the API is triggered
   useEffect(() => {
     getGroupMsgfromDB();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [buddyiesInfo.groupId, message]);
-  //useeffect for scrolling the page on new messages
+  }, [buddyiesInfo.groupId, message, getGroupMsgfromDB]);
+
   useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollIntoView({ behavior: "smooth" });
-    }
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messagesfromDB]);
 
-  //Render messages from the DB, sent in functions (senderId, message, nickname, index)
-  const renderMsg = () => {
-    return messagesfromDB.map((msgObj, ind) => {
-      return renderDM(msgObj.senderId, msgObj.message, msgObj.nickname, ind);
-    });
-  };
-  //To handle the Documents this uses only the render server and saved in the /tmp
+  const renderMsg = () =>
+    messagesfromDB.map((msgObj, ind) =>
+      renderDM(msgObj.senderId, msgObj.message, msgObj.nickname, ind)
+    );
+
   const handleDownload = async (fileUrl, fileName) => {
     try {
       const response = await fetch(`${HOST}/${fileUrl}`, {
-        withCredentials: true,
+        credentials: "include",
       });
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -78,127 +77,97 @@ const GroupMessageBody = () => {
       const blob = await response.blob();
       const url = window.URL.createObjectURL(blob);
 
-      // Create a link element to trigger the download
       const link = document.createElement("a");
       link.href = url;
       link.download = fileName;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-
-      // Open the file in a new tab
       window.open(url, "_blank");
     } catch (error) {
       console.error("Error downloading the file:", error);
     }
   };
 
-  const extractFileName = (filePath) => {
-    return filePath.split("/").pop();
-  };
+  const extractFileName = (filePath) => filePath.split("/").pop();
 
-  const renderDM = (sender, msg, nick, key) =>
-    sender === buddyiesInfo.buddyId ? (
-      <div key={key}>
-        <div className="d-flex justify-content-end mb-2">
-          <div
-            className="d-inline-block text-dark text-end bg-primary-subtle border
-            rounded-start border-info text-break p-2 mt-1"
-          >
-            {(msg.includes("uploads/files") &&
-              (/\.(jpg|png|jpeg|gif|bmp|tiff|webp|svg|ico|heic|heif)$/i.test(
-                msg
-              ) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <img src={`${HOST}/${msg}`} width="200px" height="200px" />
-                </div>
-              ) : /\.(mp4)$/i.test(msg) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <video
-                    src={`${HOST}/${msg}`}
-                    width="200px"
-                    height="200px"
-                    controls
-                  />
-                </div>
-              ) : /\.(pdf|doc|docx|xls|xlsx|ppt|pptx|txt)$/i.test(msg) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <span
-                    onClick={() => handleDownload(msg, extractFileName(msg))}
-                    style={{ display: "flex", alignItems: "center" }}
-                  >
-                    <FaFolder className="fs-5" style={{ marginRight: "8px" }} />
-                    {extractFileName(msg)}
-                    <IoDownload
-                      className="fs-5"
-                      style={{ marginLeft: "8px" }}
-                    />
-                  </span>
-                </div>
-              ) : (
-                msg
-              ))) ||
-              msg}
-          </div>
-        </div>
-      </div>
+  const renderDM = (sender, msg, nick, key) => {
+    const isSender = sender === buddyiesInfo.buddyId;
+    const messageContent = msg.includes("uploads/files") ? (
+      /\.(jpg|png|jpeg|gif|bmp|tiff|webp|svg|ico|heic|heif)$/i.test(msg) ? (
+        <img
+          src={`${HOST}/${msg}`}
+          width="200px"
+          height="200px"
+          alt=""
+          onClick={() => showImage(`${HOST}/${msg}`)}
+        />
+      ) : /\.(mp4)$/i.test(msg) ? (
+        <video src={`${HOST}/${msg}`} width="200px" height="200px" controls />
+      ) : /\.(pdf|doc|docx|xls|xlsx|ppt|pptx|txt)$/i.test(msg) ? (
+        <span
+          onClick={() => handleDownload(msg, extractFileName(msg))}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <FaFolder className="fs-5" style={{ marginRight: "8px" }} />
+          {extractFileName(msg)}
+          <IoDownload className="fs-5" style={{ marginLeft: "8px" }} />
+        </span>
+      ) : (
+        msg
+      )
     ) : (
+      msg
+    );
+
+    return (
       <div key={key}>
-        {/* Nick name is set to the message sender in the group */}
-        <div className="d-flex justify-content-start mb-2">
+        <div
+          className={`d-flex ${
+            isSender ? "justify-content-end" : "justify-content-start"
+          } mb-2`}
+        >
           <div
-            className="d-inline-block text-dark text-start bg-light border
-            rounded-end border border-secondary text-break p-2 mt-1"
+            className={`d-inline-block text-dark ${
+              isSender
+                ? "text-end bg-primary-subtle border rounded-start border-info"
+                : "text-start bg-light border rounded-end border border-secondary"
+            } text-break p-2 mt-1`}
           >
-            <div className="text-secondary d-flex justify-content-start fw-lighter group-notification-font">
-              ~
-              <span role="img" aria-label="smiling face with horns">
-                😈
-              </span>
-              {nick}
-            </div>
-            {(msg.includes("uploads/files") &&
-              (/\.(jpg|png|jpeg|gif|bmp|tiff|webp|svg|ico|heic|heif)$/i.test(
-                msg
-              ) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <img src={`${HOST}/${msg}`} width="200px" height="200px" />
+            {isSender ? (
+              messageContent
+            ) : (
+              <>
+                <div className="text-secondary d-flex justify-content-start fw-lighter group-notification-font">
+                  ~{" "}
+                  <span role="img" aria-label="smiling face with horns">
+                    😈
+                  </span>{" "}
+                  {nick}
                 </div>
-              ) : /\.(mp4)$/i.test(msg) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <video
-                    src={`${HOST}/${msg}`}
-                    width="200px"
-                    height="200px"
-                    controls
-                  />
-                </div>
-              ) : /\.(pdf|doc|docx|xls|xlsx|ppt|pptx|txt)$/i.test(msg) ? (
-                <div style={{ cursor: "pointer" }}>
-                  <span
-                    onClick={() => handleDownload(msg, extractFileName(msg))}
-                    style={{ display: "flex", alignItems: "center" }}
-                  >
-                    <FaFolder className="fs-5" style={{ marginRight: "8px" }} />
-                    {extractFileName(msg)}
-                    <IoDownload
-                      className="fs-5"
-                      style={{ marginLeft: "8px" }}
-                    />
-                  </span>
-                </div>
-              ) : (
-                msg
-              ))) ||
-              msg}
+                {messageContent}
+              </>
+            )}
           </div>
         </div>
+        {imageView !== null && (
+          <div className="position-fixed start-0 d-flex align-items-center justify-content-center custom-z-index start-50-above-425px">
+            <div className="position-relative">
+              <img src={imageView} className="img-fluid" alt="Full Size" />
+              <button
+                className="btn btn-close position-absolute top-0 end-0 m-3"
+                onClick={hideImage}
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
+  };
 
   return (
     <div className="flex-grow-1 overflow-auto p-4 px-4 custom-width scrollbar-hidden text-white">
-      {error === true ? renderMsg() : <h6>Start New Chat</h6>}
+      {error ? renderMsg() : <h6>Start New Chat</h6>}
       <div ref={scrollRef} />
     </div>
   );
