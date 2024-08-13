7fdc475ef9a4409d3882c1f50dd7821ebcd6f2a2
Rajganez <ganezsouth@live.com>
2024-08-13 17:58:06 +0530

    Added useBlocker in protected Route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,36 +1,73 @@
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { Navigate, useLocation, useBlocker } from "react-router-dom";
 import PropTypes from "prop-types";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState, useCallback } from "react";
 import PromptContext from "../context/PromptProvider";
-import { useSelector } from "react-redux";
+import CustomModal from "../components/CustomModal";
 
+// Function to check authentication status
 const isAuthenticated = () => {
   return sessionStorage.getItem("isAuthenticated") === "true";
 };
 
 const ProtectedRoute = ({ children }) => {
-  const buddyInfo = useSelector((state) => state.emailverify);
   const { activatePrompt } = useContext(PromptContext);
   const location = useLocation();
-  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [pendingTx, setPendingTx] = useState(null);
+
+  const routesToPrompt = ["/", "/buddy"];
+
+  const handleConfirm = () => {
+    setShowModal(false);
+    setPendingTx(null);
+    pendingTx.retry();
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+    setPendingTx(null);
+  };
+
+  const activatePromptLocal = useCallback((message, tx) => {
+    setModalMessage(message);
+    setShowModal(true);
+    setPendingTx(tx);
+  }, []);
+
+  useBlocker(
+    (tx) => {
+      if (isAuthenticated() && routesToPrompt.includes(tx.location.pathname)) {
+        activatePromptLocal("Are you sure you want to leave this page?", tx);
+      } else {
+        tx.retry();
+      }
+    },
+    [location.pathname, routesToPrompt, activatePromptLocal]
+  );
 
   useEffect(() => {
-    if (
-      isAuthenticated() &&
-      (location.pathname === "/" || location.pathname === "/buddy")
-    ) {
-      navigate(`/chat/${buddyInfo.buddyId}`);
-      activatePrompt("Are you sure you want to leave this page?", {
-        retry: () => {},
-      });
+    if (showModal && pendingTx) {
+      pendingTx.block();
     }
-  }, [location.pathname, activatePrompt, buddyInfo, navigate]);
+  }, [showModal, pendingTx]);
 
   if (!isAuthenticated()) {
     return <Navigate to="/" />;
   }
 
-  return children;
+  return (
+    <>
+      {children}
+      {showModal && (
+        <CustomModal
+          message={modalMessage}
+          onConfirm={handleConfirm}
+          onCancel={handleCancel}
+        />
+      )}
+    </>
+  );
 };
 
 export default ProtectedRoute;
