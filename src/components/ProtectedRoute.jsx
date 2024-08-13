c7d67135380d2ed1b4a79592179a18bc89cded85
Rajganez <ganezsouth@live.com>
2024-08-13 17:22:32 +0530

    Added navigate to stop chat page

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,27 +1,30 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useContext, useEffect } from "react";
 import PromptContext from "../context/PromptProvider";
+import { useSelector } from "react-redux";
 
 const isAuthenticated = () => {
   return sessionStorage.getItem("isAuthenticated") === "true";
 };
 
 const ProtectedRoute = ({ children }) => {
+  const buddyInfo = useSelector((state) => state.emailverify);
   const { activatePrompt } = useContext(PromptContext);
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (
       isAuthenticated() &&
       (location.pathname === "/" || location.pathname === "/buddy")
     ) {
+      navigate(`/chat/${buddyInfo.buddyId}`);
       activatePrompt("Are you sure you want to leave this page?", {
-        retry: () => {
-        },
+        retry: () => {},
       });
     }
-  }, [location.pathname, activatePrompt]);
+  }, [location.pathname, activatePrompt, buddyInfo, navigate]);
 
   if (!isAuthenticated()) {
     return <Navigate to="/" />;
